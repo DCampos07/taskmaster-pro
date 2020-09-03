41d9382f9183b5c7d2d57f7b3be9012b708ed0e2
Damaris C <DCampos07@users.noreply.github.com>
2020-09-03 19:00:32 -0400

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,7 +96,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
     // get form values
     var taskText = $("#modalTaskDescription").val();
     var taskDate = $("#modalDueDate").val();
@@ -233,18 +233,24 @@ $(".card .list-group").sortable({
     tolerance: "pointer",
     helper: "clone",
     activate: function(event) {
-
-    },
+    $(this).addClass("dropover");
+    $("bottom-trash").addClass("bottom-trash-drag"); 
+},
     deactivate: function(event) {
-
+      $(this).removeClass("dropover");
+      $("bottom-trash").removeClass("bottom-trash-drag");
     },
+    
     over: function(event) {
-
+    $(event.target).addClass("dropover-active");
+    $("bottom-trash").addClass("bottom-trash-active");
     },
+    
     out: function(event) {
-
-
+      $(event.target).removeClass("dropover-active");
+      $("bottom-trash").removeClass("bottom-trash-active");
     },
+    
     // loop over current set of children in sortable lists
     update: function(event) {
 
@@ -301,9 +307,11 @@ $("#modalDueDate").datepicker({
     minDate: 1
 });
 
-
-
-
+setInterval(function () {
+  $(".card .list-group-item").each(function (el) {
+    auditTask(el);
+  });
+}, (1000 * 60) *30);
 
 
 // load tasks for the first time
